f51614f6c229663202da25d702b053e146a01f99
Renzo Scholman <renzoscholman@gmail.com>
2018-10-22 10:35:56 +0200

    LAB1 Working memory, scheduler and testbench (for memory) and first work on registers.

--- Lab 1/registers.vhd
+++ Lab 1/registers.vhd
@@ -1,1 +1,37 @@
+library ieee;
+use ieee.math_real.all;
 
+entity registers is
+  port (store, clk, en: in bit;
+	loc: in bit_vector(3 downto 0);
+	input: in bit_vector(7 downto 0);
+	output_s, output_t: out bit_vector(7 downto 0));
+end entity registers;
+
+architecture behaviour of registers is
+    function to_integer(input : bit_vector) return integer is
+	variable output : integer;
+    begin
+	output := 0;
+	for i in input'range loop
+	    if(input(i) = '1') then
+		output := (output + 2 ** i);
+	    end if;
+	end loop;
+	return output;
+    end to_integer;
+    type register_array is array (0 to 15) of bit_vector(7 downto 0);
+    signal reg : register_array;
+begin
+    register_process: process (clk, en, store, loc, imm)
+    begin
+	if(rising_edge(clk) and en = '1') then
+		if(store = '1') then
+			reg(to_integer(loc)) <= imm;
+		else
+			output <= reg(to_integer(loc));
+		end if;
+	end if;
+ -- TODO add register declarations
+    end process;
+end registers;
--- Lab 1/scheduler.vhd
+++ Lab 1/scheduler.vhd
@@ -1,23 +1,51 @@
 library ieee;
 
+-- OP CODES:
+-- 0000 - Load immediate
+-- 0001 - Store register from memory
+-- 0010 - Load register to store in memory
+-- 0011 - Add two registers and store in third register
+
 entity scheduler is
   port (clk: in bit;
 	-- TODO is this one single 16 bit_vector value or four 4 bit_vector values?
-	op, F1, F2, F3: in bit_vector(3 downto 0););
+	op, F1, F2, F3: in bit_vector(3 downto 0));
 end entity scheduler;
 
 architecture behaviour of scheduler is
-  signal imm: bit_vector(7 downto 0);
-	 mem: bit;
-begin
-  mem <= 0;
-  if op(3 downto 0) == "0000" then
-    imm <= F2 & F3;
-    mem <= 1;
-  end if;
-  M1: memory(store=>mem, loc=>F1, imm=>imm);
-  -- TODO What is the difference in architecture for MEM and REG?
-  -- TODO registers need to remember values over multiple clock cycle?
+  component memory is
+  port (store, clk, en: in bit;
+	loc: in bit_vector(3 downto 0);
+	imm: in bit_vector(7 downto 0);
+	output: out bit_vector(7 downto 0));
+  end component;
 
-  -- TODO How to transfer ALU values back to registers (within same clock cycle)??
-end scheduler;
+  signal imm, mem_out: bit_vector(7 downto 0);
+  signal mem_loc, reg_d_loc: bit_vector(3 downto 0);
+  signal mem_store, mem_en, reg_en: bit;
+begin
+  scheduler_process: process (clk, op, F1, F2, F3)
+  begin
+    if(rising_edge(clk)) then
+      mem_store <= '0';
+      mem_en <= '0';
+      if (op(3 downto 0) = "0000") then
+	mem_en <= '1';
+	mem_loc <= F1;
+        imm <= F2 & F3;
+        mem_store <= '1';
+      end if;
+      if (op(3 downto 0) = "0001") then
+	mem_en <= '1';
+	reg_en <= '1';
+	reg_d_loc <= F1;
+	mem_loc <= F2;
+      end if;
+      -- TODO What is the difference in architecture for MEM and REG?
+      -- TODO registers need to remember values over multiple clock cycle?
+  
+      -- TODO How to transfer ALU values back to registers (within same clock cycle)??
+    end if;
+  end process scheduler_process;
+  M1: memory port map(en=>mem_en, store=>mem_store, loc=>mem_loc, clk=>clk, imm=>imm, output=>mem_out);
+end behaviour;
